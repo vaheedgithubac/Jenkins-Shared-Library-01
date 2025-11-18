148edf7c95f9e42a8f771295e5a0a65c37b0c4bb
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-18 11:49:37 +0530

    Create git_checkout.groovy

---
+++ vars/git_checkout.groovy
@@ -0,0 +1,18 @@
+def call(Map config = [:]) {
+
+    // Validate required parameters
+    if (!config.gitUrl) { error "Parameter 'gitUrl' is required" }
+    else { gitUrl = config.gitUrl }
+
+    def gitBranch        = config.gitBranch ?: 'main'
+    def gitCredentialsId = config.gitCredentialsId ?: null
+
+    // Perform Git checkout
+    git(
+        url: gitUrl,
+        branch: gitBranch,
+        credentialsId: gitCredentialsId
+    )
+    
+    echo "Checked out ${gitBranch} from ${gitUrl}"
+}
