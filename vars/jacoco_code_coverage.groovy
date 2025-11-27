00ef86786eae3863402d02ca3cfde5a89e4c58f8
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-27 10:12:55 +0530

    Update jacoco_code_coverage.groovy

--- vars/jacoco_code_coverage.groovy
+++ vars/jacoco_code_coverage.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
 
     // Validate required parameters
-
+    /*
     if (!config.jacoco_groupId) { error "Parameter 'jacoco_groupId' is required" }
     else { jacoco_groupId = config.jacoco_groupId }
 
@@ -13,10 +13,26 @@ def call(Map config = [:]) {
 
     if (!config.jacoco_goal) { error "Parameter 'jacoco_goal' is required" }
     else { jacoco_goal = config.jacoco_goal }
+    */
 
+    // Validate required parameters
+    def required = ["jacoco_groupId", "jacoco_artifactId", "jacoco_version", "jacoco_goal"]
+
+    required.each { key ->
+        if (!config[key]) {
+            error "Jacoco: Missing required parameter '${key}'"
+        }
+    }
+
+    def jacoco_groupId     = config.jacoco_groupId.trim()
+    def jacoco_artifactId  = config.jacoco_artifactId.trim()
+    def jacoco_version     = config.jacoco_version.trim()
+    def jacoco_goal        = config.jacoco_goal.trim()
+
+    echo "Running Jacoco step: ${groupId}:${artifactId}:${version}:${goal}"
     
     try { sh "mvn ${jacoco_groupId}:${jacoco_artifactId}:${jacoco_version}:${jacoco_goal}" }
-    catch (e) { error "Jacoco Maven step failed: ${e}" }
+    catch (Exception ex) { error "Jacoco Maven step failed: ${ex.message}" }
 
     // sh 'mvn org.jacoco:jacoco-maven-plugin:0.8.7:prepare-agent'
 }
