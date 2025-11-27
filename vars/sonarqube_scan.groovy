4792131154fb9f2a1c129bcc0721fface596f8e5
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-27 16:06:49 +0530

    Create sonarqube_scan.groovy

---
+++ vars/sonarqube_scan.groovy
@@ -0,0 +1,26 @@
+def call(Map config = [:]) {
+
+    // Validate required parameters
+
+    if (!config.sonarQubeAPI) { error "Parameter 'sonarQubeAPI' is required" }
+    else { sonarQubeAPI = config.sonarQubeAPI }
+
+    if (!config.scannerHome) { error "Parameter 'scannerHome' is required" }
+    else { scannerHome = config.scannerHome }
+
+    if (!config.projectName) { error "Parameter 'projectName' is required" }
+    else { projectName = config.projectName }
+
+    if (!config.projectKey) { error "Parameter 'projectKey' is required" }
+    else { projectKey = config.projectKey }
+
+    def sources      = config.sources ?: "."
+
+    withSonarQubeEnv(sonarQubeAPI) {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectName='${projectName}' \
+            -Dsonar.projectKey='${projectKey}' -X
+        """
+    }
+}
