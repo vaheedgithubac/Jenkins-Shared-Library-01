6dda214e17a290d845c4c5d1a0204bed315b2c8b
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-28 08:56:06 +0530

    Update sonarqube_scan.groovy

--- vars/sonarqube_scan.groovy
+++ vars/sonarqube_scan.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
 
     // Validate required parameters
-
+    /*
     if (!config.sonarQubeAPI) { error "Parameter 'sonarQubeAPI' is required" }
     else { sonarQubeAPI = config.sonarQubeAPI }
 
@@ -13,8 +13,27 @@ def call(Map config = [:]) {
 
     if (!config.projectKey) { error "Parameter 'projectKey' is required" }
     else { projectKey = config.projectKey }
+    */
 
     def sources      = config.sources ?: "."
+
+    def required = [
+        "sonarQubeAPI",
+        "scannerHome",
+        "projectName",
+        "projectKey"
+    ]
+
+    required.each { key ->
+        if (!config[key]) {
+            error "❌ Sonarqube: Missing required parameter '${key}'"
+        }
+    }
+
+    def sonarQubeAPI = config.sonarQubeAPI
+    def scannerHome  = config.scannerHome
+    def projectName  = config.projectName
+    def projectKey   = config.projectKey
 
     withSonarQubeEnv(sonarQubeAPI) {
         sh """
